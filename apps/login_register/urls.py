50ed7ad2a1a73a196ba6ebd36c6cca87906407c2
Jose Plascencia <jose.plascencia.eng@outlook.com>
2017-08-24 01:50:04 -0700

    front-end revamp with bootstrap

--- apps/login_register/urls.py
+++ apps/login_register/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r'^register$', views.register),
     url(r'^login$', views.login),
     url(r'^addBubbles$', views.addBubbles),
-    url(r'^wall$', views.addBubbles),
+    url(r'^wall$', views.wall),
+    url(r'^logout$', views.logout),
     url(r'^link/twitter$', views.link_twitter),
     url(r'^verification/twitter$', views.verification_twitter),
 ]
